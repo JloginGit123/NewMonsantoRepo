f1a404b510782825fdd8e2d5cb3e0c73c8f75928
163240 <163240@CTSC00513942601.cts.com>
2017-05-17 10:24:35 +0530

    IssueID::UST2 OwnerId::1000165895 entityType::AUSRST Status::Done Reviewer::admin Comments::committed on 17may-jaya

--- MonsantoJavaProject/src/MonsantoClass.java
+++ MonsantoJavaProject/src/MonsantoClass.java
@@ -3,7 +3,8 @@ public class MonsantoClass {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		System.out.println("Hello World");//Code modified for UST2
+		
 	}
 
 }
