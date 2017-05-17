c7d29ab780163f9e69350be26d361e28546faaa7
163240 <163240@CTSC00513942601.cts.com>
2017-05-17 17:34:55 +0530

    IssueID::UST3 OwnerId::1000165895 entityType::AUSRST Status::Done Reviewer::admin Comments::Developed

--- MonsantoJavaProject/src/MonsantoClass.java
+++ MonsantoJavaProject/src/MonsantoClass.java
@@ -3,7 +3,7 @@ public class MonsantoClass {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World123");//Code modified for UST2, fixed defect DEF1
+		System.out.println("Hello World1234");//Code modified for UST3, fixed defect DEF1
 		
 	}
 
