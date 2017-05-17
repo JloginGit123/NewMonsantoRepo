b27ccfe395ee133e037f8fb5eacb0466e69d9af6
163240 <163240@CTSC00513942601.cts.com>
2017-05-17 17:46:50 +0530

    IssueID::DEF2 OwnerId::1000165895 entityType::Dft Status::Done Reviewer::admin Comments::DEfect Fixed

--- MonsantoJavaProject/src/MonsantoClass.java
+++ MonsantoJavaProject/src/MonsantoClass.java
@@ -3,7 +3,7 @@ public class MonsantoClass {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World1234");//Code modified for UST3, fixed defect DEF1
+		System.out.println("Hello World12345");//Code modified for UST3, fixed defect DEF2
 		
 	}
 
