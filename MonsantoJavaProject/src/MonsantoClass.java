f02435d70ec888dc89a85c541f51ffbda46aa6c4
163240 <163240@CTSC00513942601.cts.com>
2017-05-23 16:07:58 +0530

    IssueID::UST6 OwnerId::1000165895 entityType::AUSRST Status::Done Reviewer::admin Comments::Committed by Jayashree

--- MonsantoJavaProject/src/MonsantoClass.java
+++ MonsantoJavaProject/src/MonsantoClass.java
@@ -4,6 +4,7 @@ public class MonsantoClass {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World123456fixed");//Code modified for UST5, fixed defect DEF3
+		System.out.println("Hello World1");//Code modified for UST6 
 		
 	}
 
