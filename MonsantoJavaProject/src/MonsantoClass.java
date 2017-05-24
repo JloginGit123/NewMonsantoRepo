d040d0e9272375c88c304491b4f0296d7620e847
163240 <163240@CTSC00513942601.cts.com>
2017-05-24 15:46:34 +0530

    IssueID::UST8 OwnerId::1000165895 entityType::AUSRST Status::Done Reviewer::admin Comments::Help files implemented

--- MonsantoJavaProject/src/MonsantoClass.java
+++ MonsantoJavaProject/src/MonsantoClass.java
@@ -4,7 +4,8 @@ public class MonsantoClass {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World123456fixed");//Code modified for UST5, fixed defect DEF3
-		System.out.println("Hello World1234_developed_fixed");//Code modified for UST7,defect DEF5 
+		System.out.println("Hello World1234_developed_fixed");//Code modified for UST7,defect DEF5
+		System.out.println("implemneted help files for 2 pages");//Code modified for UST8
 		
 	}
 
