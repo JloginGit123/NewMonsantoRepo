9e3ca1157a4662cc087d7da8ed06d0f6ce84e3d9
163240 <163240@CTSC00513942601.cts.com>
2017-05-25 14:24:12 +0530

    IssueID::DEF2 OwnerId::1000165895 entityType::Dft Status::Done Reviewer::admin Comments::Fixed_PraTest

--- MonsantoJavaProject/src/MonsantoClass.java
+++ MonsantoJavaProject/src/MonsantoClass.java
@@ -6,7 +6,7 @@ public class MonsantoClass {
 		System.out.println("Hello World123456fixed");//Code modified for UST5, fixed defect DEF3
 		System.out.println("Hello World1234_developed_fixed");//Code modified for UST7,defect DEF5
 		System.out.println("implemneted help files for 2 pages");//Code modified for UST8,fixed Defect 6
-		System.out.println("implemneted help files for 1 pages1");//Code modified for UST1_pratest
+		System.out.println("implemneted help files for 1 pages1");//Code modified for UST1_pratest,fixed Defect 2_pratest
 		
 	}
 
