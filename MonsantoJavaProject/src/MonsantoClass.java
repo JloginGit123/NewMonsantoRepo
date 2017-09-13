172661eb09650ad3b5b9794f244ec421439fe2d6
163240 <163240@CTSC00513942601.cts.com>
2017-09-13 09:44:22 +0530

    IssueID::UST5614 OwnerId::1000165895 entityType::AUSRST Status::Done Reviewer::admin Comments::Committed by Jayashree on 13Sep

--- MonsantoJavaProject/src/MonsantoClass.java
+++ MonsantoJavaProject/src/MonsantoClass.java
@@ -8,7 +8,7 @@ public class MonsantoClass {
 		System.out.println("implemneted help files for 2 pages");//Code modified for UST8,fixed Defect 6
 		System.out.println("implemneted help files for 1 pages1");//Code modified for UST1_pratest,fixed Defect 2_pratest
 		System.out.println("implemneted export for excel in page2");//Code modified for UST8,fixed Defectfixed
-		System.out.println("Test SB12");//Code modified for UST5613,fixed Defectfixed
+		System.out.println("Test SB123");//Code modified for UST5613,fixed Defectfixed
 		
 	}
 
